5da4e2d1d80303b131046ddcbda887ca7960dd64
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-10-24 16:07:37 -0300

    created component OrderItem and page Cart

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,13 +3,17 @@ import styled from "styled-components";
 export const Container = styled.button`
   width: 100%;
   height: 4.8rem;
-
+  
   border: none;
   border-radius: 0.8rem;
   background: ${({ theme }) => theme.COLORS.RED};
   color: ${({ theme }) => theme.COLORS.WHITE};
   font-size: 1.4rem;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1.1rem;
   &:disabled {
     opacity: 0.5;
     cursor: not-allowed;
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -14,7 +14,6 @@ export const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  
   > button {
     display: flex;
     justify-content: center;
@@ -28,7 +27,6 @@ export const Content = styled.div`
       font-size: 2.2rem;
     }
   }
-
   @media (min-width: 768px) {
     padding-inline: 12.3rem;
     > button {
@@ -44,19 +42,19 @@ export const Nav = styled.div`
   right: 0;
   bottom: 0;
   z-index: 5;
+  height: 40rem;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
   padding-inline: 4rem;
   padding-top: 5rem;
   opacity: ${({ isVisible }) => isVisible ? 1 : 0};
   visibility: ${({ isVisible }) => isVisible ? 'visible' : 'hidden'};
-  transform: ${({ isVisible }) => isVisible ? 'translateY(0)' :'translateY(100%)'};
+  transform: ${({ isVisible }) => isVisible ? 'translateY(0)' :'translateY(-20%)'};
   display: flex;
   flex: 1;
   flex-direction: column;
   align-items: center;
   gap: 3.2rem;
   transition: all 0.4s;
-
   @media (min-width: 768px) {
     position: relative;
     top: 0;
@@ -91,12 +89,10 @@ export const Search = styled.div`
   border-radius: 0.5rem;
   padding-right: 1.6rem;
   position: relative;
-
   > svg {
     left: 1.6rem;
     position: absolute;
   }
-
   > input {
     width: 100%;
     height: 4.8rem;
@@ -110,7 +106,6 @@ export const Search = styled.div`
       color: ${({ theme }) => theme.COLORS.GRAY_500};
     }
   }
-
   @media (min-width: 768px) {
     flex: 1;
   }
@@ -127,7 +122,6 @@ export const Button = styled.a`
   background-color: ${({ theme }) => theme.COLORS.RED};
   color: ${({ theme }) => theme.COLORS.WHITE};
   font-size: 1.4rem;
-  
   @media (min-width: 768px) {
     width: 21.6rem;
   }
@@ -137,5 +131,5 @@ export const Logout = styled.a`
   > svg {
     font-size: 2.2rem;
     color: ${({ theme }) => theme.COLORS.WHITE};
-  }
-`; 
+  } 
+`
